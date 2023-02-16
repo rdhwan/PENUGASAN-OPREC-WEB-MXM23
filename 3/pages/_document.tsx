6869cfdfab580814d2e8141c0384e882ee6d2ad7
rdhwan <fathanridhwann@gmail.com>
2023-02-16 22:44:49 +0700

    [update]: change page title

--- 3/pages/_document.tsx
+++ 3/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="quotify. | generate your quotes"/>
       <body className="bg-green-100">
         <div className="bg-index bg-fill md:bg-cover bg-fixed bg-right md:bg-center bg-no-repeat">
           <Main />
